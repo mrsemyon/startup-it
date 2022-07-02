360c165e4736381413b4465ffb094f7e1dadf6b4
mrSemyon <syoma4k@gmail.com>
2022-07-02 22:36:00 +0300

    Moved header parts from files to header.php

--- create-category.php
+++ create-category.php
@@ -1,42 +1,7 @@
-<!doctype html>
-<html lang="ru">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-utilities.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Категории</title>
-</head>
-<body>
-<div class="main-wrapper">
-    <div class="content">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">Test</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Материалы</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Теги</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Категории</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container">
+<?php
+$title = 'Категории';
+require $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
+?>
             <h1 class="my-md-5 my-4">Добавить категорию</h1>
             <div class="row">
                 <div class="col-lg-5 col-md-8">
---
+++ templates/header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-utilities.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title><?= $title ?></title>
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <div class="content">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+                    <a class="navbar-brand" href="#">Test</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/list-material.php">Материалы</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/list-tag.php">Теги</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/list-category.php">Категории</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="container">
